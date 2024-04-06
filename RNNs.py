baf53621d7706da96208d1c1770263a6be5c6198
Nguyễn Dũng <121219639+nqdhocai@users.noreply.github.com>
2024-04-06 21:50:54 +0700

    Add files via upload

---
+++ RNNs.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn as nn
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.h2o(hidden)
+        output = self.softmax(output)
+
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+class GRU(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(GRU, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        # Reset gate parameters
+        self.reset_gate = nn.Linear(input_size + hidden_size, hidden_size)
+        self.reset_activation = nn.Sigmoid()
+
+        # Update gate parameters
+        self.update_gate = nn.Linear(input_size + hidden_size, hidden_size)
+        self.update_activation = nn.Sigmoid()
+
+        # New memory proposal parameters
+        self.memory_proposal = nn.Linear(input_size + hidden_size, hidden_size)
+        self.memory_activation = nn.Tanh()
+
+        # Output layer
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        # Concatenate input and hidden state
+        combined = torch.cat((input, hidden), 1)
+
+        # Calculate reset gate
+        reset = self.reset_gate(combined)
+        reset = self.reset_activation(reset)
+
+        # Calculate update gate
+        update = self.update_gate(combined)
+        update = self.update_activation(update)
+
+        # Calculate new memory proposal
+        memory = self.memory_proposal(combined)
+        memory = self.memory_activation(memory)
+
+        # Update hidden state
+        hidden = torch.mul((1 - reset), hidden) + torch.mul(reset, memory)
+
+        # Output layer
+        output = self.h2o(hidden)
+        output = self.softmax(output)
+
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(LSTM, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        # Forget gate parameters
+        self.forget_gate = nn.Linear(input_size + hidden_size, hidden_size)
+        self.forget_activation = nn.Sigmoid()
+
+        # Input gate parameters
+        self.input_gate = nn.Linear(input_size + hidden_size, hidden_size)
+        self.input_activation = nn.Sigmoid()
+
+        # Update parameters
+        self.update_gate = nn.Linear(input_size + hidden_size, hidden_size)
+        self.update_activation = nn.Tanh()
+
+        # Output gate parameters
+        self.output_gate = nn.Linear(input_size + hidden_size, hidden_size)
+        self.output_activation = nn.Sigmoid()
+
+        # Output layer
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden_state, cell_state):
+        # Concatenate input and hidden state
+        combined = torch.cat((input, hidden_state), 1)
+
+        # Forget gate
+        forget = self.forget_gate(combined)
+        forget = self.forget_activation(forget)
+
+        # Input gate
+        input_gate = self.input_gate(combined)
+        input_gate = self.input_activation(input_gate)
+
+        # Update gate
+        update = self.update_gate(combined)
+        update = self.update_activation(update)
+
+        # Update cell state
+        cell_state = torch.mul(forget, cell_state) + torch.mul(input_gate, update)
+
+        # Output gate
+        output_gate = self.output_gate(combined)
+        output_gate = self.output_activation(output_gate)
+
+        # Calculate hidden state
+        hidden_state = torch.mul(output_gate, torch.tanh(cell_state))
+
+        # Output layer
+        output = self.h2o(hidden_state)
+        output = self.softmax(output)
+
+        return output, hidden_state, cell_state
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+    def initCellState(self):
+        return torch.zeros(1, self.hidden_size)
---
+++ tokenizer.py
@@ -0,0 +1,81 @@
+import collections
+
+class Vocab:
+    def __init__(self, corpus, min_freq=0, token='word'):
+        tokens = self.tokenize(corpus, token=token)
+        reserved_tokens = []
+        # Sort according to frequencies
+        counter = self.count_corpus(tokens)
+        self.token_freqs = sorted(counter.items(), key=lambda x: x[0])
+        self.token_freqs.sort(key=lambda x: x[1], reverse=True)
+        self.unk, uniq_tokens = 0, ['<unk>'] + reserved_tokens
+        uniq_tokens += [token for token, freq in self.token_freqs
+                        if freq >= min_freq and token not in uniq_tokens]
+        self.idx_to_token, self.token_to_idx = [], dict()
+        for token in uniq_tokens:
+            self.idx_to_token.append(token)
+            self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+    def count_corpus(self, sentences):
+        # Flatten a list of token lists into a list of tokens
+        tokens = [tk for line in sentences for tk in line]
+        return collections.Counter(tokens)
+
+    def tokenize(self, corpus, token='word'):
+        """Split sentences into word or char tokens."""
+        if token == 'word':
+            return [sentence.split(' ') for sentence in corpus]
+        elif token == 'char':
+            return [list(sentence) for sentence in corpus]
+        else:
+            print('ERROR: unknown token type ' + token)
+
+
+class Tokenizer:
+    def __init__(self, vocab=None, token='word', max_length=32):
+        self.token = token
+        self.max_length = max_length
+        if vocab != None:
+            self.vocab = vocab
+        else:
+            # cần một corpus để tạo vocab cố định cho token, tuỳ theo từng field mong muốn để chọn corpus phù hợp
+            # needed a corpus to create a fixed vocab, depending on each field which was desiderated to choose appropriate corpus
+            self.vocab = Vocab(corpus=[], token=token)
+
+    def tokenize(self, corpus, token='word'):
+        """Split sentences into word or char tokens."""
+        if token == 'word':
+            return [sentence.split(' ') for sentence in corpus]
+        elif token == 'char':
+            return [list(sentence) for sentence in corpus]
+        else:
+            print('ERROR: unknown token type ' + token)
+
+    def transform(self, sentences):
+        tokens = self.tokenize(sentences, token=self.token)
+        encode_tokens = []
+        for token in tokens:
+            temp = self.vocab[token]
+            if len(temp) >= self.max_length:
+                temp = temp[:self.max_length]
+                encode_tokens.append(temp)
+                continue
+
+            else:
+                temp.extend([0]*(self.max_length-len(temp)))
+                encode_tokens.append(temp)
+        return encode_tokens
+
